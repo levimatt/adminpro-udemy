76c7b2ba680bd368ce8a2ad1f2e0744916bc2dce
Fernando <fernando.herrera85@gmail.com>
2018-05-25 08:53:38 -0600

    Hasta aquí todo bien

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,7 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    const obs = new Observable( (observer: Subscriber<number>) => {
+
+      let contador = 0;
+
+      const intervalo = setInterval( () => {
+
+        contador++;
+
+        observer.next(contador);
+
+
+        if ( contador === 3 ) {
+          clearInterval(intervalo);
+          observer.complete();
+        } 
+
+        if ( contador === 2 ) {
+          observer.error('Auxilio!');
+        }
+
+      }, 1000 );
+
+    });
+
+
+    obs.subscribe(
+      numero => console.log('Subs', numero),
+      error => console.error('Error en el obs', error ),
+      () => console.log('El observador termino!')
+    );
+
+  }
 
   ngOnInit() {
   }
